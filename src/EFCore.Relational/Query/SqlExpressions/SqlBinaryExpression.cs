2ab7ebab8b69dacd010e79b82345104610572d3e
Andrea Canciani <ranma42@gmail.com>
2024-12-23 10:21:30 +0100

    eager

--- src/EFCore.Relational/Query/SqlExpressions/SqlBinaryExpression.cs
+++ src/EFCore.Relational/Query/SqlExpressions/SqlBinaryExpression.cs
@@ -14,6 +14,7 @@ namespace Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 /// </summary>
 public class SqlBinaryExpression : SqlExpression
 {
+    private readonly int _hashCode;
     private static ConstructorInfo? _quotingConstructor;
 
     /// <summary>
@@ -42,6 +43,8 @@ public class SqlBinaryExpression : SqlExpression
         OperatorType = operatorType;
         Left = left;
         Right = right;
+
+        _hashCode = ComputeHashCode();
     }
 
     /// <summary>
@@ -160,6 +163,7 @@ public class SqlBinaryExpression : SqlExpression
         => obj != null
             && (ReferenceEquals(this, obj)
                 || obj is SqlBinaryExpression sqlBinaryExpression
+                && _hashCode == sqlBinaryExpression._hashCode
                 && Equals(sqlBinaryExpression));
 
     private bool Equals(SqlBinaryExpression sqlBinaryExpression)
@@ -170,5 +174,8 @@ public class SqlBinaryExpression : SqlExpression
 
     /// <inheritdoc />
     public override int GetHashCode()
+        => _hashCode;
+
+    private int ComputeHashCode()
         => HashCode.Combine(base.GetHashCode(), OperatorType, Left, Right);
 }
